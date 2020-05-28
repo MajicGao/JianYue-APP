4a2605a2ce9baa2518519f13ce63912c890b2906
MajicGao <1039932706@qq.com>
2020-05-28 08:21:01 +0800

    creat project

---
+++ app/src/androidTest/java/com/zscdumin/zhixinapp/activity/UserLoginActivityTest.java
@@ -0,0 +1,14 @@
+package com.zscdumin.zhixinapp.activity;
+
+import org.junit.Test;
+
+/**
+ * Created by ZSCDumin on 2018/4/4.
+ * 作者邮箱：2712220318@qq.com
+ */
+public class UserLoginActivityTest {
+
+    @Test
+    public void isLoginSucess() {
+    }
+}
